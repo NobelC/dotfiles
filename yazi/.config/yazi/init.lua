afdf052f02efdf7a9e13c12610e4cdd36955aa80
NobelC <jeremynobel16@gmail.com>
2026-09-08 20:31:48 -0500

    fix: remove invalid tab_width from yazi theme.toml

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,21 +1,16 @@
--- ~/dotfiles/yazi/.config/yazi/init.lua
+-- Yazi init.lua - Configuración de inicialización
 
--- Enable shell integration for cd on quit
+-- Linemode personalizado: muestra tamaño y fecha de modificación
 function Linemode:size_and_mtime()
-  local time = math.floor(self.file.cha.mtime or 0)
-  if time == 0 then
-    time = ""
-  elseif os.date("%Y", time) == os.date("%Y") then
-    time = os.date("%b %d %H:%M", time)
-  else
-    time = os.date("%b %d  %Y", time)
-  end
+    local time = math.floor(self.file.cha.mtime or 0)
+    if time == 0 then
+        time = ""
+    elseif os.date("%Y", time) == os.date("%Y") then
+        time = os.date("%b %d %H:%M", time)
+    else
+        time = os.date("%b %d  %Y", time)
+    end
 
-  local size = self.file:size()
-  return string.format("%s %s", size and ya.readable_size(size) or "-", time)
+    local size = self.file:size()
+    return string.format("%s %s", size and ya.readable_size(size) or "-", time)
 end
-
--- Custom linemode showing file size and modification time
-ya.manager_emit("setup", {
-  linemode = "size_and_mtime",
-})
